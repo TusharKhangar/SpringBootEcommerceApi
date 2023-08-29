8dc55ecd8df37aa3f715fabaadf55eeee92cafb1
TusharKhangar <tusharkhangar7490@gmail.com>
2023-08-30 01:52:09 +0530

    Fully updated to the user registration and signup

---
+++ ecom-backend/src/main/java/com/ecom/config/Configuration.java
@@ -0,0 +1,11 @@
+package com.ecom.config;
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
+
+    @org.springframework.context.annotation.Configuration
+    public class Configuration {
+//        @Bean
+//        public ModelMapper modelMapperBean() {
+//            return new ModelMapper();
+//        }
+    }
